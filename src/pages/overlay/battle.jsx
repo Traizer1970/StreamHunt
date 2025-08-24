72dea59ecbec4152e31aeaa76f861b45b55df063
Traizer1970 <guisousa512@gmail.com>
2025-08-24 02:24:36 +0100

    Widgets map

---
+++ src/pages/overlay/battle.jsx
@@ -0,0 +1,7 @@
+// src/pages/overlay/battle.jsx
+import React from "react";
+import WidgetOverlay from "../../widget-overlay.jsx";
+
+export default function BattleOverlayPage() {
+  return <WidgetOverlay />;
+}
